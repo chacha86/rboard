b557a79d22918af2acac0088fedd1aa604348eb2
chacha86 <taejincha0619@gmail.com>
2024-05-21 17:52:41 +0900

    jwt start

---
+++ src/main/java/com/example/reactboard/domain/auth/AuthController.java
@@ -0,0 +1,9 @@
+package com.example.reactboard.domain.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+}
---
+++ src/main/java/com/example/reactboard/domain/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,12 @@
+package com.example.reactboard.domain.auth.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+}
